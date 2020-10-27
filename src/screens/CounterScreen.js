7520a77ef0b5b04e7383a7ba26ad912d0a64e853
Jean-Yves Lalande <jylalande@yahoo.com>
2020-10-26 21:56:02 -0600

    After implementing reducer. Did not need to do that just for demo

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,25 +1,44 @@
-import React, {useState} from "react"; // usestate is a hook to follow the state of the variable
+import React, { useReducer } from "react"; // usestate is a hook to follow the state of the variable
 import { Text, StyleSheet, View, Button } from "react-native";
 
-const CounterScreen = ({navigation}) => {  // stands for props.navigation
-    
-    const[counter, setCounter] = useState(0); // creating a counter with a state - array destructuring[]
+
+const reducer = (state, action) => {
+
+  switch (action.type) { // does not change the state object. Returns a copy with the modified data
+    case 'increase_counter':
+      console.log(action.payload);
+      return state.counter + action.payload > 255
+        ? state
+        : { ...state, counter: state.counter + action.payload };
+    case 'decrease_counter':
+      console.log(action.payload);
+      return state.counter - action.payload < 0
+        ? state
+        : { ...state, counter: state.counter - action.payload };
+
+    default:
+      return state;
+  };
+};
+
+const CounterScreen = ({ navigation }) => {  // stands for props.navigation
+
+  //const[counter, setCounter] = useState(0); // creating a counter with a state - array destructuring[]
+  const [state, dispatch] = useReducer(reducer, { counter: 0 }); // intitialize the reducer with the starting values
+
   return (
-      <View>
-          <Text style={styles.text}>Counter</Text>
-          <Button title="Increase" onPress={() => {
-              //Don't do this
-              //counter++;
-              setCounter(counter + 1);
-          }}
-          />
-          <Button title="Decrease"  onPress={() => {
-              //counter--;
-              setCounter(counter - 1);
-          }}
-          />
-          <Text style={styles.text} >{counter}</Text>
-      </View>
+    <View>
+      <Text style={styles.text}>Counter</Text>
+      <Button title="Increase" onPress={() => {
+        dispatch({ type: 'increase_counter', payload: 1 });
+      }}
+      />
+      <Button title="Decrease" onPress={() => {
+        dispatch({ type: 'decrease_counter', payload: 1 });
+      }}
+      />
+      <Text style={styles.text} >{state.counter}</Text>
+    </View>
   );
 };
 
